909b59b8f40b2fe96ef3843ff4996ddb07a9ff9e
Lahiru Dissanayake <lahirudissanayake15@gmail.com>
2026-06-08 09:43:34 +0530

    Refactor CreateCollectionDialog and collection service for improved readability

--- components/collections/create-collection-dialog.tsx
+++ components/collections/create-collection-dialog.tsx
@@ -44,9 +44,7 @@ export function CreateCollectionDialog() {
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger>
-        <Button>New Collection</Button>
-      </DialogTrigger>
+      <DialogTrigger render={<Button>New Collection</Button>} />
 
       <DialogContent>
         <DialogHeader>
--- services/collection.service.ts
+++ services/collection.service.ts
@@ -2,23 +2,19 @@ import { api } from "@/lib/axios";
 
 export const collectionService = {
   getAll: async () => {
-    const response =
-      await api.get("/collections");
+    const response = await api.get("/collections");
 
     return response.data;
   },
 
-  create: async (
-    data: {
-      name: string;
-      description?: string;
-    }
-  ) => {
-    const response =
-      await api.post(
-        "/collections",
-        data
-      );
+  getById: async (id: string) => {
+    const response = await api.get(`/collections/${id}`);
+
+    return response.data;
+  },
+
+  create: async (data: { name: string; description?: string }) => {
+    const response = await api.post("/collections", data);
 
     return response.data;
   },
